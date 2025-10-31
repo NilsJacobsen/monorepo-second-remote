ecada8518f1855788c03e29491e803828f5f5ea6
Nils Jacobsen <nils.jacobsen98@gmail.com>
2025-10-31 13:45:17 +0100

    initial file structure

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,5 +2,6 @@ import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
   './packages/fs/vitest.config.ts',
+  './packages/sdk-react/vitest.config.ts',
   // add all package test configs here
 ]);
