e30cbbecd9e9e94fce12b6b064f923bfb425b8f2
Nils Jacobsen <nils.jacobsen98@gmail.com>
2025-11-20 16:26:14 +0100

    fix(assistant-ui): setup

---
+++ examples/ai-form-example/eslint.config.mjs
@@ -0,0 +1,19 @@
+// export { default } from "@assistant-ui/x-buildutils/eslint";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
+
+export default eslintConfig;
