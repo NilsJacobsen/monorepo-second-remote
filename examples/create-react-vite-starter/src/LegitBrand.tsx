c709f92c9dc2a4fc585bdfe9b90fe4035fe63896
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2025-12-16 13:04:48 +0100

    update vite legit brand

--- examples/create-react-vite-starter/src/LegitBrand.tsx
+++ examples/create-react-vite-starter/src/LegitBrand.tsx
@@ -3,26 +3,28 @@ type HeaderProps = { handleShare: () => void };
 export const LegitHeader = ({ handleShare }: HeaderProps) => {
   return (
     <div className="brand-header">
-      <div className="brand-logos">
-        <a href="https://legitcontrol.com" aria-label="Legit">
-          <img alt="Legit Logo" src="/logo.svg" width={70} height={40} />
-        </a>
-        <span className="brand-divider">x</span>
-        <a
-          href="https://vite.dev"
-          aria-label="Vite"
-          className="brand-logo-pair"
+      <div className="brand-header__inner">
+        <div className="brand-logos">
+          <a href="https://legitcontrol.com" aria-label="Legit">
+            <img alt="Legit Logo" src="/logo.svg" width={70} height={40} />
+          </a>
+          <span className="brand-divider">x</span>
+          <a
+            href="https://vite.dev"
+            aria-label="Vite"
+            className="brand-logo-pair"
+          >
+            <img alt="Vite Logo" src="/vite.svg" width={30} height={24} />
+            <img alt="React Logo" src="/react.png" width={28} height={24} />
+          </a>
+        </div>
+        <button
+          onClick={handleShare}
+          className="primary-button primary-button--dark"
         >
-          <img alt="Vite Logo" src="/vite.svg" width={30} height={24} />
-          <img alt="React Logo" src="/react.png" width={28} height={24} />
-        </a>
+          Share
+        </button>
       </div>
-      <button
-        onClick={handleShare}
-        className="primary-button primary-button--dark"
-      >
-        Share
-      </button>
     </div>
   );
 };
@@ -36,19 +38,21 @@ export const LegitFooter = () => {
 
   return (
     <div className="brand-footer">
-      <p className="brand-footer__note">
-        This is made with{' '}
-        <a className="brand-footer__highlight" href="https://legitcontrol.com">
-          Legit
-        </a>
-        {' <3'}
-      </p>
-      <div className="brand-footer__links">
-        {links.map(link => (
-          <a key={link.href} href={link.href} className="brand-footer__link">
-            {link.label}
+      <div className="brand-footer__inner">
+        <p className="brand-footer__note">
+          This is made with{' '}
+          <a className="brand-footer__highlight" href="https://legitcontrol.com">
+            Legit
           </a>
-        ))}
+          {' <3'}
+        </p>
+        <div className="brand-footer__links">
+          {links.map(link => (
+            <a key={link.href} href={link.href} className="brand-footer__link">
+              {link.label}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
